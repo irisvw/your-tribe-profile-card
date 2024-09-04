92da080623aedc9ca42bf2335bb2c19f9ddebc28
Iris van Willigen <iris.van.willigen@gmail.com>
2024-09-04 12:18:43 +0200

    Remove unused classes

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,6 @@
 const spinButton = document.querySelector('button')
-// const articles = document.querySelectorAll('article')
 
 spinButton.addEventListener('click', spin)
-
-// function spin() {
-//   articles.forEach((article) => article.classList.toggle("spin"));
-// }
 
 const main = document.querySelector('main')
 
