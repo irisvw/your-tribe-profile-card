8ac73a24c249bd210cc9fa32fd1ef6e94a643577
Iris van Willigen <iris.van.willigen@gmail.com>
2024-09-04 11:57:47 +0200

    Overhaul

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const spinButton = document.querySelector('button')
+// const articles = document.querySelectorAll('article')
 
-function expand () {
-  document.body.classList.toggle('expand')
+spinButton.addEventListener('click', spin)
+
+// function spin() {
+//   articles.forEach((article) => article.classList.toggle("spin"));
+// }
+
+const main = document.querySelector('main')
+
+function spin() {
+  main.classList.toggle("spin");
 }
